b60c17147699befc0212ac0eeadcf82726469fe0
Adrian Aymont <aaymont@gmail.com>
2026-02-14 12:06:15 -0500

    Fixes

--- addins/safety-dashboard/vite.config.ts
+++ addins/safety-dashboard/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ubiquitous-barnacle/addins/safety-dashboard/",
+  base: "./",
   build: {
     outDir: "dist",
     emptyOutDir: true,
